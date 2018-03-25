687a13a12d233b878f82dd0ba794a42fb7b30653
Alice Di Nunno <alice@dinunno.fr>
2018-03-26 00:15:51 +0200

    Added HormoneType Enum

---
+++ transtracker/src/HormoneType.swift
@@ -0,0 +1,15 @@
+//
+//  HormoneType.swift
+//  transtracker
+//
+//  Created by Alice Di Nunno on 25/03/2018.
+//  Copyright © 2018 Alice Di Nunno. All rights reserved.
+//
+
+import Foundation
+enum HormoneType: Int {
+    case HT_Estradiol = 0
+    case HT_Testosterone
+    case HT_TestosteroneBlocker
+    case HT_Progesterone
+}
